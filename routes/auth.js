04cc5731eeb2b4006408d9dbcf71096b73818dc9
Afolabi <afolabiogunbanwo@gmail.com>
2025-10-21 17:26:48 +0100

    added /me to route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,14 @@
 import  express from "express";
 import { authenticateUser } from "../middlewares/authMiddleware";
-import { registerUser,saveNameAndUsername,loginUser,forgotPassword,verifyToken,resetPassword,checkPhone,sendOtpToEmail,verifyOtp} from "../controllers/authController";
+import { registerUser,loginUser,forgotPassword,verifyToken,resetPassword,getLoggedInUser,sendOtpToEmail,verifyOtp} from "../controllers/authController";
 
 const router = express.Router()
 
 router.route('/register').post(registerUser);
-router.route('/login').post(loginUser); 
+router.route('/login').post(loginUser);
+
+router.route('/me').get(authenticateUser,getLoggedInUser);
+// router.route('/verify/mobile/:phoneNumber').get(checkPhone); 
 
 router.route('/email/send').post(sendOtpToEmail);
 
