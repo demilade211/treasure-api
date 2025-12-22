93e4158558e860cbf8092c68eb01148cac97e7d4
Afolabi <afolabiogunbanwo@gmail.com>
2025-12-22 05:41:29 +0100

    added callback url

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -87,6 +87,7 @@ export const initializeOrderPayment = async (req, res, next) => {
                 deliveryPrice,
                 totalPrice,
             },
+            callback_url: `https://treasure-box-hazel.vercel.app/profile`
         };
 
         const response = await axios.post(
