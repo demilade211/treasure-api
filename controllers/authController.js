981a9578675cee56e578e64186183649b8e664f1
Afolabi <afolabiogunbanwo@gmail.com>
2026-01-15 04:27:06 +0100

    added return to send otp

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,7 +59,7 @@ export const sendOtpToEmail = async (req, res, next) => {
 
             await user.save({ validateBeforeSave: false });
 
-            await handleEmail(user,next,message)
+            return await handleEmail(user,next,message)
 
         }
 
